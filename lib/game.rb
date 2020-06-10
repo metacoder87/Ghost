be92809b59d2617fb2ad6d637a274bc9fd445e28
metacoder87 <gary.miller@gmx.com>
2020-06-09 20:19:29 -0400

    Creates and initializes the Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,23 @@
             # meta_coder (Gary Miller) =)
             # gmiller052611@gmail.com
             # https://github.com/metacoder87/Ghost
+
+
+require_relative 'player'
+            
+class Game
+
+    attr_accessor :players, :fragment, :rec, :dictionary
+
+    def initialize(*players)
+        @players = []
+        players.each { |player| @players << Player.new(player) }
+        @fragment = ""
+        @dictionary = Set.new
+        IO.readlines('/Volumes/2tXD Backup/My Stuff/Documents/App Academy Assignments/gary_miller_ghost_project/lib/dictionary.txt').each do |line|
+            @dictionary.add(line.chomp)
+        end
+        @rec = Hash.new()
+        @players.each { |player| @rec[player] = "" }        
+    end
+
