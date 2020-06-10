38adeea51608519d11337a9e9816cd4f3fd50b80
metacoder87 <gary.miller@gmx.com>
2020-06-09 20:21:48 -0400

    Creates the Player class and adds in initialize

---
+++ lib/player.rb
@@ -0,0 +1,12 @@
+            # meta_coder (Gary Miller) =)
+            # gmiller052611@gmail.com
+            # https://github.com/metacoder87/Ghost
+
+
+class Player
+
+    attr_accessor :player_name
+
+    def initialize(player_name)
+        @player_name = player_name
+    end
