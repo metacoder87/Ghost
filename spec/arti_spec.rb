feb91c01f3ec291330552fa120fcf1d640d070e5
metacoder87 <gary.miller@gmx.com>
2020-06-15 12:49:17 -0400

    Creates the arti spec file

---
+++ spec/arti_spec.rb
@@ -0,0 +1,11 @@
+            # meta_coder (Gary Miller) =)
+            # gmiller052611@gmail.com
+            # https://github.com/metacoder87/Ghost
+
+
+require 'arti'
+
+describe "Arti" do
+    let(:arti) { Arti.new("ai_1") }
+    let(:game) { Game.new("ai_1") }
+
