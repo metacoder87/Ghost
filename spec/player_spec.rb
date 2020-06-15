340d115295abf150b7a1b9afbd939160371fbe81
metacoder87 <gary.miller@gmx.com>
2020-06-15 12:52:11 -0400

    Adds in the initialize spec method

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -8,3 +8,13 @@ require 'player'
 describe "Player" do
     let(:player_1) { Player.new("player_1") }
 
+    describe "#initialize" do
+        it "should accept a player name" do
+            player_1
+        end
+    end
+
+    describe "#get_guess" do
+        it "gets a guess from the player" do
+            input = double(:x, :chomp=>"x")
+            allow(player_1).to receive(:gets).and_return(input)
