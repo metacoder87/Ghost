d89cb0deff0aeac9dd44a324c769bd16cd978ad1
metacoder87 <gary.miller@gmx.com>
2020-06-15 12:52:33 -0400

    Adds in the get_guess spec method

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -18,3 +18,12 @@ describe "Player" do
         it "gets a guess from the player" do
             input = double(:x, :chomp=>"x")
             allow(player_1).to receive(:gets).and_return(input)
+
+        expect { player_1.get_guess }.to output(/player_1 take a guess.../).to_stdout
+        end
+    end
+end
+
+            # meta_coder (Gary Miller) =)
+            # gmiller052611@gmail.com
+            # https://github.com/metacoder87/Ghost
