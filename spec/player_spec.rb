c71dc0669e4d9baa43c0e5d39f5816c494ce38c1
metacoder87 <gary.miller@gmx.com>
2020-06-15 12:51:50 -0400

    Creates the player spec file

---
+++ spec/player_spec.rb
@@ -0,0 +1,10 @@
+            # meta_coder (Gary Miller) =)
+            # gmiller052611@gmail.com
+            # https://github.com/metacoder87/Ghost
+
+
+require 'player'
+
+describe "Player" do
+    let(:player_1) { Player.new("player_1") }
+
