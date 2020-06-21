00aaf7106a2776a6638722ee02f041d65dd8f23a
metacoder87 <gary.miller@gmx.com>
2020-06-20 21:02:28 -0400

    Updates the file opener

--- lib/game.rb
+++ lib/game.rb
@@ -16,7 +16,7 @@ class Game
         players.each { |player| player[0..2] == "ai_" || player[0..2] == "art" ? @players << Arti.new(player) : @players << Player.new(player)}
         @fragment = ""
         @dictionary = Set.new
-        IO.readlines('/Volumes/2tXD Backup/My Stuff/Documents/App Academy Assignments/gary_miller_ghost_project/lib/dictionary.txt').each do |line|
+        IO.readlines('lib/dictionary.txt').each do |line|
             @dictionary.add(line.chomp)
         end
         @rec = Hash.new()
